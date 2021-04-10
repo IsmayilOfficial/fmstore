df6ce9913aa9cd844c20c9f55e3c34db6976c6d0
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-10 14:11:02 +0400

    sette opp vuex/state

--- djackets_vue/src/store/index.js
+++ djackets_vue/src/store/index.js
@@ -2,6 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart:{
+      items: []
+    },
+    isAuthentifacated: false,
+    token: '',
+    isLoading: false
+
   },
   mutations: {
   },
