5ec39741c4f15aae70759fdf1cbd6f12de1131cb
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-11 03:08:29 +0400

    cart

--- djackets_vue/src/components/ProductBox.vue
+++ djackets_vue/src/components/ProductBox.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="collumn is-3">
-       <div class="box">
-         <figure class="image mb-4">
-           <img :src="product.get_thumbnail">
-         </figure>
-         <h3 class="is-size-4">{{product.name}}</h3>
-         <p class="is-size-6 has-text-grey">${{product.price}}</p>
+    <div class="column is-3">
+        <div class="box">
+            <figure class="image mb-4">
+                <img v-bind:src="product.get_thumbnail">
+            </figure>
 
-         <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-       </div>
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-    
-   </div>
+            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+        </div>
+    </div>
 </template>
+
 <script>
 export default {
     name: 'ProductBox',
-    props:{
+    props: {
         product: Object
     }
 }
