2f86bb4b2bd322e6a6dab571f55cd771255157b1
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-10 23:45:55 +0400

    search functionality

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,15 +1,17 @@
+from django.db.models import Q
 from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Product, Category
-from .serializers import ProductSerializer,CategorySerializer
+from .serializers import ProductSerializer, CategorySerializer
 
 class LatestProductsList(APIView):
-    def get(self, request, fomat=None):
+    def get(self, request, format=None):
         products = Product.objects.all()[0:4]
-        serializer = ProductSerializer(products, many = True)
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
 class ProductDetail(APIView):
@@ -23,6 +25,7 @@ class ProductDetail(APIView):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
 class CategoryDetail(APIView):
     def get_object(self, category_slug):
         try:
@@ -34,3 +37,14 @@ class CategoryDetail(APIView):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
