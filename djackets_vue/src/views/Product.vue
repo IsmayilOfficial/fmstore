c520906c978a493c7aa12976b3426e87371cfe51
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-10 16:09:02 +0400

    loading bar

--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -32,50 +32,46 @@
 
 
 <script>
-
 import axios from 'axios'
-import {toast} from 'bulma-toast'
+import { toast } from 'bulma-toast'
 export default {
-  name: 'Product',
-  data(){
-      return{
-          product: {},
-          quantity: 1
-
-      }
-  },
-  mounted(){
-      this.getProduct()
-  },
-  methods: {
-      getProduct(){
-          const category_slug = this.$route.params.category_slug
-          const product_slug = this.$route.params.product_slug
-
-          axios
-           .get(`/api/v1/products/${category_slug}/${product_slug}`)
+    name: 'Product',
+    data() {
+        return {
+            product: {},
+            quantity: 1
+        }
+    },
+    mounted() {
+        this.getProduct() 
+    },
+    methods: {
+        async getProduct() {
+            this.$store.commit('setIsLoading', true)
+            const category_slug = this.$route.params.category_slug
+            const product_slug = this.$route.params.product_slug
+            await axios
+                .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
                     this.product = response.data
-
                     document.title = this.product.name + ' | Djackets'
                 })
                 .catch(error => {
                     console.log(error)
                 })
-      },
-      addToCart(){
-          if(isNaN(this.quantity) || this.quantity < 1 ) {
-              this.quantity = 1
-          }
-
-          const item = {
-              product: this.product,
-              quantity: this.quantity
-          }
-
-          this.$store.commit('addToCart',item)
-
-         toast({
+            
+            this.$store.commit('setIsLoading', false)
+        },
+        addToCart() {
+            if (isNaN(this.quantity) || this.quantity < 1) {
+                this.quantity = 1
+            }
+            const item = {
+                product: this.product,
+                quantity: this.quantity
+            }
+            this.$store.commit('addToCart', item)
+            toast({
                 message: 'The product was added to the cart',
                 type: 'is-success',
                 dismissible: true,
@@ -83,9 +79,7 @@ export default {
                 duration: 2000,
                 position: 'bottom-right',
             })
-
-     }
-  }
-
+        }
+    }
 }
 </script>
